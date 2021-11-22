88ff900d1778a38c9be03befd938169597d882bd
NozomiSG <1762513582@qq.com>
2021-11-23 02:25:16 +0800

    new project

--- Git/COMP2411/src/Application.java
+++ Git/COMP2411/src/Application.java
@@ -1,56 +1,66 @@
-//import java.util.Objects;
-//import java.util.Scanner;
-//
-//public class Application {
-//
-//    public static void main(String[] args) {
-//
-//    }
-//
-//
-//
-//    public static void homepage() {
-//        String n;
-//        /**
-//         * You should input account before your registration
-//         */
-//        System.out.println("Welcome use Grade Book system! ");
-//        System.out.println("Register an account      >>> Enter(1)");
-//        System.out.println("Login your account       >>> Enter(2)");
-//        System.out.println("Quit the program         >>> Enter(3)");
-//        boolean flag = true;
-//        String scan;
-//        while (flag) {
-//            Scanner scanner = new Scanner(System.in);
-//            if (scanner.hasNext()) {
-//                scan = scanner.next();
-//                scanner.close();
-//                if (scan.equals("1")||scan.equals("2")||scan.equals("3")) {
-//                    scan
-//                    flag = false;
-//                }
-//                else
-//                    System.out.println("Wrong input, please try again!");
-//            }
-//        }
-//        switch (scan) {
-//
-//            case "1" -> {
-//            }
-//            case "2" -> {
-//            }
-//            case "3" -> {
-//            }
-//        }
-//
-//
-//        if (n.equals("R") || n.equals("r")) registerAccount();
-//        else if (n.equals("L") || n.equals("l")) login();
-//        else if (n.equals("I") || n.equals("i")) input();
-//        else if (n.equals("~")) cout << "Bye!" << endl;
-//        else {
-//            System.out.println("Your instruction is wrong, please re-enter");
-//            homepage();
-//        }
-//    }
-//}
+import java.util.Scanner;
+
+public class Application {
+
+    public static void main(String[] args) {
+        homepage();
+    }
+
+    public static void registerAccount() {
+        String username, password, phoneNumber;
+        Scanner scanner = new Scanner(System.in);
+
+    }
+    public static void loginAccount() {
+
+    }
+    public static void loginAdmin() {
+
+    }
+    public static void checkDelivery() {
+
+    }
+
+    public static void homepage() {
+        System.out.println("Welcome use deliverApp!");
+        System.out.println("\n=====================================");
+        System.out.println("Register an account      >>> Enter(1)");
+        System.out.println("Login your account       >>> Enter(2)");
+        System.out.println("Check delivery status    >>> Enter(3)");
+        System.out.println("Login admin account      >>> Enter(4)");
+        System.out.println("Exit the program         >>> Enter(5)");
+        System.out.println("=====================================\n\n\n");
+        int scan;
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("Please enter your command: ");
+            try {
+                scan = scanner.nextInt();
+                if (scan > 0 && scan < 6)
+                    break;
+                else
+                    System.out.println("Your enter is wrong, please enter again!");
+            }catch(Exception e) {
+                System.out.println("Your enter is wrong, please enter again!");
+                scanner.next();
+            }
+        }
+        switch (scan) {
+            case 1 ->
+                registerAccount();
+            case 2 ->
+                loginAccount();
+            case 3 ->
+                checkDelivery();
+            case 4 ->
+                loginAdmin();
+            case 5 ->
+                System.out.println("Bye");
+            default -> {
+                System.out.println("Unknown command, please enter again!\n\n\n\n");
+                homepage();
+            }
+        }
+    }
+}
+
